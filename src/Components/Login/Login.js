440e7f8e00df79e83ba7dd6a89866c8f37a9e4c2
MUHAMMADSabbirHossain <dustobalok000@gmail.com>
2023-09-24 22:51:24 +0600

    login from added...

--- src/Components/Login/Login.js
+++ src/Components/Login/Login.js
@@ -1,9 +1,30 @@
 import React from 'react';
+import { Button, Form } from 'react-bootstrap';
 
 const Login = () => {
     return (
-        <div>
-            login
+        <div md="w-25" className='container w-50'>
+            <h2 className='text-secondary'>Please Login</h2>
+            <Form>
+                <Form.Group className="mb-3" controlId="formBasicEmail">
+                    <Form.Label className=''>Email address</Form.Label>
+                    <Form.Control type="email" placeholder="Enter email" />
+                    <Form.Text className="text-muted">
+                        We'll never share your email with anyone else.
+                    </Form.Text>
+                </Form.Group>
+
+                <Form.Group className="mb-3" controlId="formBasicPassword">
+                    <Form.Label>Password</Form.Label>
+                    <Form.Control type="password" placeholder="Password" />
+                </Form.Group>
+                <Form.Group className="mb-3" controlId="formBasicCheckbox">
+                    <Form.Check type="checkbox" label="Check me out" />
+                </Form.Group>
+                <Button variant="primary" type="submit">
+                    Submit
+                </Button>
+            </Form>
         </div>
     );
 };
